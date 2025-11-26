bdd861456d5258d4de9fa8fcc774141577f11c42
ShivamKumar1234116 <shivamkumar39917@gmail.com>
2025-11-27 00:47:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
    origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:8080",
+    // "http://localhost:5173",
+    // "http://localhost:5174",
+    // "http://localhost:8080",
     "https://tripvilla-pq6o.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
